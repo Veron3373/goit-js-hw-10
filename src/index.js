8a65111a2203973f1cdc385997ecee417fb422ce
Veron3373 <veron3373v@gmail.com>
2022-11-07 14:19:28 +0200

    +

--- src/index.js
+++ src/index.js
@@ -12,57 +12,56 @@ const countryInfo = document.querySelector('.country-info');
 countryInput.addEventListener('input', debounce(inputCountry, DEBOUNCE_DELAY));
 
 function inputCountry(evt) {
-    const nameCountry = evt.target.value.trim();
-    if (nameCountry === "") {
-        return;
-    }
-fetchCountries(nameCountry)
+  const nameCountry = evt.target.value.trim();
+  if (nameCountry === "") {
+    return;
+  }
+  fetchCountries(nameCountry)
     .then(response => {
-        if (response.length > 10) {
-            Notify.info(
-                'Too many matches found. Please enter a more specific name.'
-            );
-        }
-        if (response.length >= 2 && response.length <= 10) {
-            searchListCountry(response);
-        }
-        if (response.length === 1) {
-            searchOneCountry(response)
-        }
+      if (response.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (response.length >= 2 && response.length <= 10) {
+        searchListCountry(response);
+      }
+      if (response.length === 1) {
+        searchOneCountry(response)
+      }
     })
     .catch(error => console.log(error));
-    clearSearchCountry();
-    }
+  clearSearchCountry();
+}
 
 function searchListCountry(response) {
-    const markup = response
-        .map(el => {
-            return `<li class="item__country">
+  const markup = response
+    .map(el => {
+      return `<li class="item__country">
             <img class="img" src="${el.flags.svg}" width = 30 alt="flag">
             <h3 class="title">${el.name.official}</h3>
             </li>`;
-        })
-        .join('');
-    countryList.innerHTML = markup;
+    })
+    .join('');
+  countryList.innerHTML = markup;
 }
 
-    
+
 function searchOneCountry(response) {
-    const markup = response
+  const markup = response
     .map(el => {
-    return `<div class="item__country"><img class="img" src="${
-        el.flags.svg
-    }" width=50 alt="flag">
+      return `<div class="item__country"><img class="img" src="${el.flags.svg
+        }" width=50 alt="flag">
     <h1 class ="title">${el.name.official}</h1></div>
     <p class="text"><b>Capital:</b> ${el.capital}</p>
     <p class="text"><b>Population:</b> ${el.population}</p>
     <p class="text"><b>Languages:</b> ${Object.values(el.languages)}</p>`;
     })
     .join('');
-    countryInfo.innerHTML = markup;
+  countryInfo.innerHTML = markup;
 }
 
 function clearSearchCountry() {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
 }
